93495a03f75ed420dc23f1be7ddaa89bc57320ef
René Anderes <randeres@yahoo.de>
2018-10-01 19:07:02 +0200

    Setting

--- cookbookApp/src/main/java/android/anderes/org/cookbook/gui/SettingsActivity.java
+++ cookbookApp/src/main/java/android/anderes/org/cookbook/gui/SettingsActivity.java
@@ -3,19 +3,25 @@ package android.anderes.org.cookbook.gui;
 import android.anderes.org.cookbook.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import java.util.List;
 
-public class SettingsActivity extends Activity {
+
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Display the fragment as the main content.
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
+        if (savedInstanceState == null) {
+            // Display the fragment as the main content.
+            getFragmentManager().beginTransaction()
+                    .replace(android.R.id.content, new SettingsFragment())
+                    .commit();
+        }
     }
 
 }
