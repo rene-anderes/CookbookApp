5d5c062106bb9838263b8e8b5a737e55a7251d52
René Anderes <randeres@yahoo.de>
2018-10-08 13:58:21 +0200

    JobScheduler eingebaut

--- cookbookApp/src/main/java/android/anderes/org/cookbook/AppUtilities.java
+++ cookbookApp/src/main/java/android/anderes/org/cookbook/AppUtilities.java
@@ -56,19 +56,24 @@ class AppUtilities implements AppConfiguration {
     public void scheduleJob() {
         final ComponentName serviceComponent = new ComponentName(context, CookbookSyncJobService.class);
         final JobInfo.Builder builder = new JobInfo.Builder(SYNC_JOB_ID, serviceComponent);
-        // schedule the start of the service every 10 - 30 seconds
-        builder.setMinimumLatency(1 * 1000); // wait at least
-        builder.setOverrideDeadline(3 * 1000); // maximum delay
-        //builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED); // require unmetered network
+        builder.setPeriodic(10 * 1000) // every 10 seconds
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED); // require unmetered network
         //builder.setRequiresDeviceIdle(true); // device should be idle
         //builder.setRequiresCharging(false); // we don't care if the device is charging or not
         final JobScheduler jobScheduler = (JobScheduler)context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
         jobScheduler.schedule(builder.build());
+        for (JobInfo info : jobScheduler.getAllPendingJobs()) {
+            Log.v("Sync", "Job: " + info.getService());
+        }
     }
 
     @Override
     public void cancelJob() {
         final JobScheduler jobScheduler = (JobScheduler)context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        assert jobScheduler != null;
         jobScheduler.cancel(SYNC_JOB_ID);
+        for (JobInfo info : jobScheduler.getAllPendingJobs()) {
+            Log.v("Sync", "Job: " + info.getService());
+        }
     }
 }
--- cookbookApp/src/main/java/android/anderes/org/cookbook/service/CookbookSyncJobService.java
+++ cookbookApp/src/main/java/android/anderes/org/cookbook/service/CookbookSyncJobService.java
@@ -2,14 +2,34 @@ package android.anderes.org.cookbook.service;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import static android.anderes.org.cookbook.AppConstants.BROADCAST_SYNC_ACTION;
 
 public class CookbookSyncJobService extends JobService {
+
+    private BroadcastReceiver receiver;
 
     @Override
     public boolean onStartJob(JobParameters params) {
 
-        Intent service = new Intent(getApplicationContext(), CookbookSyncService.class);
+        final Intent service = new Intent(getApplicationContext(), CookbookSyncService.class);
+
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                jobFinished(params, true);
+                LocalBroadcastManager.getInstance(context).unregisterReceiver(this);
+            }
+        };
+
+        final IntentFilter intentFilter = new IntentFilter(BROADCAST_SYNC_ACTION);
+        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(receiver, intentFilter);
         getApplicationContext().startService(service);
 
         return true;
@@ -19,4 +39,5 @@ public class CookbookSyncJobService extends JobService {
     public boolean onStopJob(JobParameters params) {
         return true;
     }
+
 }
