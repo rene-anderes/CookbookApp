94732384bc7dfb75011fe0415106efba70bcb49b
René Anderes <randeres@yahoo.de>
2019-03-25 12:30:23 +0100

    Update Libs, kleiner Bug behoben

--- cookbookApp/src/main/java/org/anderes/app/cookbook/gui/SettingsActivity.java
+++ cookbookApp/src/main/java/org/anderes/app/cookbook/gui/SettingsActivity.java
@@ -19,23 +19,31 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (savedInstanceState == null) {
-
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-            SharedPreferences.OnSharedPreferenceChangeListener listener =
-                    (sharedPrefs, key) -> {
-                        if (key.equalsIgnoreCase("pref_backgroundSync")) {
-                            final Boolean backgroundSync = sharedPrefs.getBoolean("pref_backgroundSync", FALSE);
-                            Log.d(LOG_TAG_SYNC, "SharedPreferences - Key: pref_backgroundSync - Result: " + backgroundSync);
-                            if (backgroundSync) {
-                                ServiceLocatorProvider.getInstance().getAppConfiguration().scheduleJob();
-                            } else {
-                                ServiceLocatorProvider.getInstance().getAppConfiguration().cancelJob();
-                            }
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.OnSharedPreferenceChangeListener listener =
+                (sharedPrefs, key) -> {
+                    if (key.equalsIgnoreCase("pref_backgroundSync")) {
+                        final Boolean backgroundSync = sharedPrefs.getBoolean("pref_backgroundSync", FALSE);
+                        Log.d(LOG_TAG_SYNC, "SharedPreferences - Key: pref_backgroundSync - Result: " + backgroundSync);
+                        if (backgroundSync) {
+                            ServiceLocatorProvider.getInstance().getAppConfiguration().scheduleJob();
+                        } else {
+                            ServiceLocatorProvider.getInstance().getAppConfiguration().cancelJob();
                         }
-                    };
-            prefs.registerOnSharedPreferenceChangeListener(listener);
+                    }
+                };
+        prefs.registerOnSharedPreferenceChangeListener(listener);
 
+        // savedInstanceState is non-null when there is fragment state
+        // saved from previous configurations of this activity
+        // (e.g. when rotating the screen from portrait to landscape).
+        // In this case, the fragment will automatically be re-added
+        // to its container so we don't need to manually add it.
+        // For more information, see the Fragments API guide at:
+        //
+        // http://developer.android.com/guide/components/fragments.html
+        //
+        if (savedInstanceState == null) {
             // Display the fragment as the main content.
             final Fragment prefFragment = new SettingsFragment();
 
